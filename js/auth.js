fae7a0be46f972491f59ed8eb52dda20d4704376
Marjansiswanto <80183739+Marjansiswanto@users.noreply.github.com>
2026-07-06 17:37:16 +0800

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,34 @@
+async function login() {
 
+    const username =
+        document.getElementById("username").value;
+
+    const password =
+        document.getElementById("password").value;
+
+    const response =
+        await fetch("config/users.json");
+
+    const data =
+        await response.json();
+
+    const user =
+        data.users.find(u =>
+            u.username === username &&
+            u.password === password
+        );
+
+    if (user) {
+
+        sessionStorage.setItem(
+            "spieUser",
+            JSON.stringify(user)
+        );
+
+        window.location.href =
+            "dashboard.html";
+
+    }
+    else {
+
+        document.getElementById("
