810f34ab1c3c760edbceda2ecdee21bc09d3a5bd
Marjansiswanto <80183739+Marjansiswanto@users.noreply.github.com>
2026-07-07 05:44:45 +0800

    Update sidebar.js

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,23 @@
+document.getElementById("sidebar").innerHTML = `
+<h2>SPIE</h2>
 
+<hr>
+
+<ul>
+
+<li>🏠 Dashboard</li>
+
+<li>📄 Documents</li>
+
+<li>⬆ Upload PDF</li>
+
+<li>🔍 Search</li>
+
+<li>⚙ Settings</li>
+
+<li>📜 Activity</li>
+
+<li onclick="logout()">🚪 Logout</li>
+
+</ul>
+`;
